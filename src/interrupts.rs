16d955e676a6a3878f89ac8bd6fe8960f120b862
robertwenhk <robertwen.hk@gmail.com>
2024-03-17 21:56:25 +0800

    add page fault handler for interrupts

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,6 @@
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
-use crate::{println, print, gdt};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
+use crate::{println, print, gdt, hlt_loop};
 use pic8259::ChainedPics;
 use spin;
 
@@ -20,6 +20,8 @@ static ref IDT: InterruptDescriptorTable = {
         }
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
+
         idt
         };
 }
@@ -70,6 +72,18 @@ extern "x86-interrupt" fn keyboard_interrupt_handler(_stack_frame: InterruptStac
     unsafe {
         PICS.lock().notify_end_of_interrupt(InterruptIndex::Keyboard.as_u8());
     }
+}
+
+extern "x86-interrupt" fn page_fault_handler(stack_frame: InterruptStackFrame,
+                                             error_code: PageFaultErrorCode) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("ACCESSING ADDRESS: {:?}", Cr2::read());
+    println!("ERROR CODE: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+
+    hlt_loop();
 }
 
 #[derive(Clone, Copy, Debug)]
--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,12 @@ pub extern "C" fn _start() -> ! {
 
     rustos::init();
 
+    // new
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
     #[cfg(test)]
     test_main();
 
